2c2744166bf8e6d09951e87edd85313efa32d789
league <league@wts20.attlocal.net>
2018-06-22 18:25:24 -0700

    Change Calculator Test Score

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,17 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog(null,"How many nickels do u have with you right now?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int nickelz = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog(null,"How many dimes do u have with you right now?");
 		// Ask the user how many quarters they have, and convert their answer
-
+int dimez = Integer.parseInt(dimes);
 		// Calculate how much money the user has and save it in a double variable 
-
+double monies=dimez*.1+nickelz*.05;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null,"You have "+monies);
 	}
 }
 
