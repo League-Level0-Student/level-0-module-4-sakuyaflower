0f1e825cbaeb087561cc4d4f3615e16ae1487aa3
league <league@wts20.attlocal.net>
2018-07-06 18:26:16 -0700

    Pi, String Method, Happy Pet

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -1,26 +1,43 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
+	public static void main(String[] args) {
+		// 1. Make a main method and make sure your program can run
 
-	// 1. Make a main method and make sure your program can run
+		// 2. Make a String variable to hold the value of Pi. You could use
+		// http://www.piday.org/million/ for the value.
+		String pi = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384";
+		// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second
+		// is "pi.charAt(1)"
+		System.out.println(pi.charAt(0));
+		System.out.println(pi.charAt(1));
+		// 4. Print ALL the digits of of Pi (hint: use a loop)
+		for (int i = 0; i < pi.length(); i++) {
+			char answer = getInputFromUser();
+			if (pi.charAt(i) == answer) {
+				System.out.println("Ur not an idiot XD");
+			}
+			else {
+			System.out.println("Ur an idiot");	
+			}
+			speak(pi.charAt(i));
+			System.out.println(pi.charAt(i));
+		}
+		
+		// 5. Use the speak() method to speak all the digits of Pi.
 
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
+		// [ADVANCED]
+		// *6. Get a character from the user using the getInputFromUser() method
 
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+		// *7. Compare the users' char to the next digit of Pi
 
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
-	// 5. Use the speak() method to speak all the digits of Pi.
-
-	// [ADVANCED]
-	// *6. Get a character from the user using the getInputFromUser() method
-	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+		// *8. If they are correct, print out "correct". If they are not, print
+		// "incorrect" to System.err.println
+	}
 
 	static void speak(char characterToSpeak) {
 		try {
@@ -38,12 +55,9 @@ public class PiAloud {
 		}
 	}
 
-
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
 
 }
-
-
